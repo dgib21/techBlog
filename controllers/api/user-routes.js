c72967a52e393c87849e5ff3c11e39e3b20e672d
Devin Gibson <dgib21@hotmail.com>
2020-11-09 12:36:33 -0600

    updated error message

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,7 +43,7 @@ router.get("/:id", (req, res) => {
   })
     .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
+        res.status(404).json({ message: "Invalid Credentials" });
         return;
       }
       res.json(dbUserData);
